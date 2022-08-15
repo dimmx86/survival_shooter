1924f4b616b2d36c736ce9501fa3479716a347d2
Dmitriy Moskalenko <dimmx86@gmail.com>
2022-08-15 16:30:26 +0700

    Joystick

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -4,5 +4,13 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-   
+    public float speed;
+    public FloatingJoystick _joystick;
+    public Rigidbody2D rb;
+
+    public void FixedUpdate()
+    {
+        Vector3 direction = Vector3.up * _joystick.Vertical + Vector3.right * _joystick.Horizontal;
+        rb.velocity = (direction * speed * Time.fixedDeltaTime);
+    }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private Mover _mover;
+    [SerializeField] private Rigidbody2D _rigidbody;
 
 
     public Mover Mover => _mover;
+    public Rigidbody2D RootRigidbody;
 }
 
 
